42b618dc1a9e5d231b552b4c0bcec6273143aa37
kaaus-tubh <1999kaukulk@gmail.com>
2021-09-21 20:37:56 +0530

    user dao updated

--- src/main/java/com/demo/dao/userDao/UserDaoImpl.java
+++ src/main/java/com/demo/dao/userDao/UserDaoImpl.java
@@ -1,39 +1,153 @@
 package com.demo.dao.userDao;
 
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.demo.beans.User;
-import com.demo.exceptions.userExceptions.EmailAlreadyExistException;
+import com.demo.dbutil.DBUtil;
+import com.demo.exceptions.userExceptions.EmailAlreadyExistsException;
 import com.demo.exceptions.userExceptions.UserNotFoundException;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
 	@Override
-	public int RegisterUser(User user) throws EmailAlreadyExistException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public User registerUser(User per) throws EmailAlreadyExistsException {
+		try {
+			Connection conn = DBUtil.getConnConnection();
 
-	@Override
-	public int LoginUser(String email, String password) throws EmailAlreadyExistException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+			PreparedStatement pst = conn.prepareStatement(
+					"insert into user(userName, userRole, userTelephone, userEmail, userPassword) values(?,?,?,?,?)");
+			pst.setString(1, per.getUserName());
+			pst.setString(2, per.getUserRole());
+			pst.setString(3, per.getUserTelephone());
+			pst.setString(4, per.getUserEmail());
+			pst.setString(5, per.getUserPassword());
+			int count = pst.executeUpdate();
 
-	@Override
-	public int uploadArrayOfUsers(User[] users) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public User getUserById(int id) throws UserNotFoundException {
-		// TODO Auto-generated method stub
+			if (count == 1) {
+				return per;
+			}
+		} catch (SQLException e) {
+			throw new EmailAlreadyExistsException(
+					"Cannot add! User with email " + per.getUserEmail() + " already exists !");
+		}
 		return null;
 	}
 
 	@Override
-	public User[] getAllUsers() throws UserNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public User loginUser(String email, String pswd) throws UserNotFoundException {
+		User user = null;
+		try {
+			Connection conn = DBUtil.getConnConnection();
+
+			PreparedStatement pst = conn.prepareStatement("select * from user where userEmail=? and userPassword=?");
+
+			pst.setString(1, email);
+			pst.setString(2, pswd);
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				user = new User(rs.getInt("userId"), rs.getString("userName"), rs.getString("userRole"),
+						rs.getString("userTelephone"), rs.getString("userEmail"), rs.getString("userPassword"));
+			}
+		} catch (SQLException e) {
+			throw new UserNotFoundException("No such user found !");
+		}
+		return user;
+	}
+
+	@Override
+	public int importUser(String path) throws Exception {
+		int status = 0;
+		try {
+			List<User> users = getJSONtoList(path);
+			try {
+				for (User u : users) {
+					registerUser(u);
+				}
+			} catch (EmailAlreadyExistsException e) {
+				throw new EmailAlreadyExistsException(
+						"Cannot import ! Some/All users with same email already exists !");
+			}
+			status = 1;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		return status;
+	}
+
+	private List<User> getJSONtoList(String path) throws Exception {
+		List<User> userList = new ArrayList<User>();
+		try {
+			JSONParser jsonParser = new JSONParser();
+			JSONArray arr = (JSONArray) jsonParser.parse(new FileReader(path));
+
+			for (Object o : arr) {
+
+				JSONObject user = (JSONObject) o;
+				String name = (String) user.get("userName");
+				String role = (String) user.get("userRole");
+				String phone = (String) user.get("userTelephone");
+				String email = (String) user.get("userEmail");
+				String pswd = (String) user.get("userPassword");
+
+				userList.add(new User(name, role, phone, email, pswd));
+			}
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+		return userList;
+	}
+
+	@Override
+	public List<User> displayAllUser() {
+		List<User> list = new ArrayList<>();
+		User per = null;
+		try {
+			Connection conn = DBUtil.getConnConnection();
+			PreparedStatement pst = conn.prepareStatement("select * from user");
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				per = new User(rs.getInt("userId"), rs.getString("userName"), rs.getString("userRole"),
+						rs.getString("userTelephone"), rs.getString("userEmail"), rs.getString("userPassword"));
+				list.add(per);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public User displayUserWithId(int id) throws UserNotFoundException {
+
+		User per = null;
+		try {
+			Connection conn = DBUtil.getConnConnection();
+			PreparedStatement pst = conn.prepareStatement("select * from user where userId = ?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+
+			if (rs.next()) {
+				per = new User(rs.getInt("userId"), rs.getString("userName"), rs.getString("userRole"),
+						rs.getString("userTelephone"), rs.getString("userEmail"), rs.getString("userPassword"));
+			} else
+				throw new UserNotFoundException("No Person found !");
+		} catch (SQLException e) {
+			throw new UserNotFoundException("No Person found !");
+		}
+		return per;
 	}
 
 }
