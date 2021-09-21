c68e36a9bd41cf903dafbcea0e6244a36c48bc75
AnjaliKhandelwal27 <57789765+AnjaliKhandelwal27@users.noreply.github.com>
2021-09-21 11:58:32 +0530

    methods updated

--- src/main/java/com/demo/dao/assetDao/AssetDao.java
+++ src/main/java/com/demo/dao/assetDao/AssetDao.java
@@ -8,24 +8,24 @@ import com.demo.exceptions.assetExceptions.AssetNotFoundException;
 
 public interface AssetDao {
 	
-	// method that will enable admin to add asset
-		public int addAsset(Asset a);
+		// method that will enable admin to add asset
+		public void addAsset(Asset a);
 		
 		// method that will search an IT asset by name
 		public Asset searchAssetByName(String name) throws AssetNotFoundException;
 		
 		// method that will search an IT asset by date
-		public Asset searchAssetByDate(Date date) throws AssetNotFoundException;
+		public List<Asset> searchAssetByDate(Date date) throws AssetNotFoundException;
 		
 		// method that will search an It asset by category
-		public Asset searchAssetByCategory(String category) throws AssetNotFoundException;
+		public List<Asset> searchAssetByCategory(String category) throws AssetNotFoundException;
 		
-		// method that will take user and Asset data as an input and issue an asset
-		public int issueAsset(User user,Asset asset) throws AssetNotFoundException;
+		
 		// method to getAllAssets 
-		public Asset[] getAllAssets();
+		public List<Asset> getAllAssets();
+	
+		//update asset quantity if it already exists
+		public void updateAsset(Asset a);
 		
-		// method that enable admin to add an asset
-		public int AddAssetAdmin(Asset a);
 
 }
