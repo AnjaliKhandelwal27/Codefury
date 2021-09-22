d96cf078f690786c22722e443bf31a5c0ae643f9
AnjaliKhandelwal_27 <anjalikhandelwal589@gmail.com>
2021-09-22 19:42:24 +0530

    static block & date format used

--- src/main/java/com/demo/beans/Asset.java
+++ src/main/java/com/demo/beans/Asset.java
@@ -1,24 +1,25 @@
 package com.demo.beans;
 
+import java.time.LocalDate;
+
+
 public class Asset {
 	
 	
 	private String assetName , assetType ,assetDescription;
-	private String assetDateAdded;
+	private LocalDate assetDateAdded;
 	private int assetAvailable ,assetLendingPeriod;
 	private float assetLateReturnFee ;
 	private int assetBannedDays;
 	
-	
 	public Asset() {
+		super();
 		
 	}
 
-
-	public Asset( String assetName, String assetType, String assetDescription, String assetDateAdded,
+	public Asset(String assetName, String assetType, String assetDescription, LocalDate assetDateAdded,
 			int assetAvailable, int assetLendingPeriod, float assetLateReturnFee, int assetBannedDays) {
-		
-		
+		super();
 		this.assetName = assetName;
 		this.assetType = assetType;
 		this.assetDescription = assetDescription;
@@ -28,105 +29,81 @@ public class Asset {
 		this.assetLateReturnFee = assetLateReturnFee;
 		this.assetBannedDays = assetBannedDays;
 	}
-
-
-	
-
-
-	
-
 
 	public String getAssetName() {
 		return assetName;
 	}
 
-
 	public void setAssetName(String assetName) {
 		this.assetName = assetName;
 	}
-
 
 	public String getAssetType() {
 		return assetType;
 	}
 
-
 	public void setAssetType(String assetType) {
 		this.assetType = assetType;
 	}
-
 
 	public String getAssetDescription() {
 		return assetDescription;
 	}
 
-
 	public void setAssetDescription(String assetDescription) {
 		this.assetDescription = assetDescription;
 	}
 
-
-	public String getAssetDateAdded() {
+	public LocalDate getAssetDateAdded() {
 		return assetDateAdded;
 	}
 
-
-	public void setAssetDateAdded(String assetDateAdded) {
+	public void setAssetDateAdded(LocalDate assetDateAdded) {
 		this.assetDateAdded = assetDateAdded;
 	}
-
 
 	public int getAssetAvailable() {
 		return assetAvailable;
 	}
 
-
 	public void setAssetAvailable(int assetAvailable) {
 		this.assetAvailable = assetAvailable;
 	}
-
 
 	public int getAssetLendingPeriod() {
 		return assetLendingPeriod;
 	}
 
-
 	public void setAssetLendingPeriod(int assetLendingPeriod) {
 		this.assetLendingPeriod = assetLendingPeriod;
 	}
-
 
 	public float getAssetLateReturnFee() {
 		return assetLateReturnFee;
 	}
 
-
 	public void setAssetLateReturnFee(float assetLateReturnFee) {
 		this.assetLateReturnFee = assetLateReturnFee;
 	}
-
 
 	public int getAssetBannedDays() {
 		return assetBannedDays;
 	}
 
-
 	public void setAssetBannedDays(int assetBannedDays) {
 		this.assetBannedDays = assetBannedDays;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Asset [ assetName=" + assetName + ", assetType=" + assetType
-				+ ", assetDescription=" + assetDescription + ", assetDateAdded=" + assetDateAdded + ", assetAvailable="
-				+ assetAvailable + ", assetLendingPeriod=" + assetLendingPeriod + ", assetLateReturnFee="
-				+ assetLateReturnFee + ", assetBannedDays=" + assetBannedDays + "]";
+		return "Asset [assetName=" + assetName + ", assetType=" + assetType + ", assetDescription=" + assetDescription
+				+ ", assetDateAdded=" + assetDateAdded + ", assetAvailable=" + assetAvailable + ", assetLendingPeriod="
+				+ assetLendingPeriod + ", assetLateReturnFee=" + assetLateReturnFee + ", assetBannedDays="
+				+ assetBannedDays + "]";
 	}
 	
 	
 	
 	
-
+	
 }
-
--- src/main/java/com/demo/dao/assetDao/AsssetDaoImpl.java
+++ src/main/java/com/demo/dao/assetDao/AsssetDaoImpl.java
@@ -1,44 +1,74 @@
 package com.demo.dao.assetDao;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 import com.demo.beans.Asset;
 import com.demo.beans.User;
 import com.demo.exceptions.assetExceptions.AssetNotFoundException;
 
-public class AsssetDaoImpl implements AssetDaoIntf {
+/* 
+ * when you call add method from service class convert the date into LocalDate n pass it in this format
+ * LocalDate.of(YYYY/MM/DD)
+ * i.e create Asset obj as
+ * new Asset("Lenevo","Laptop","Its an dell laptop",LocalDate.of(2021,9,12),1,14,25,10);
+ * */
 
-	Connection conn = null;
-	
-	
-	public AssetDaoImpl() {
+public class AssetDaoImpl implements AssestDaoIntf {
 
-		conn = ConnectionFactory.getDBConnection();
+	static Connection conn=null;
+	static PreparedStatement pinsert;
+	static PreparedStatement pselectCategory;
+	static PreparedStatement pselectName;
+	static PreparedStatement pselectAll;
+	static PreparedStatement pselectDate;
+	
+	static {
+		
+		try {
+			conn = ConnectionFactory.getDBConnection();
+			pinsert=conn.prepareStatement("Insert into Asset ( assetName,assetType,assetDescription,assetDateAdded,assetAvailability,assetLendingPeriod,"
+					+ "assetLateReturnFee,assetBannedDays) values(?,?,?,?,?,?,?,?)");
+			pselectAll=conn.prepareStatement("Select * from Asset");
+			pselectName=conn.prepareStatement("Select * from Asset where assetName=?");
+			pselectCategory=conn.prepareStatement("Select * from Asset where assetType=?");
+			pselectDate=conn.prepareStatement("Select * from Asset where assetDateAdded=?");
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
-
+	
+	
+	
 	// for adding asset
 	@Override
 	public  void addAsset(Asset a) throws SQLException {
 
-		PreparedStatement ps = null;
+		//used LocalDate to create the date in asset class
+		// for database java.sql.date is used
+		Date date = Date.valueOf(a.getAssetDateAdded());
+           
+			pinsert.setString(1, a.getAssetName());
+			pinsert.setString(2, a.getAssetType());
+			pinsert.setString(3, a.getAssetDescription());
+			pinsert.setDate(4,date);
+			pinsert.setInt(5, a.getAssetAvailable());
+			pinsert.setInt(6, a.getAssetLendingPeriod());
+			pinsert.setFloat(7, a.getAssetLateReturnFee());
+			pinsert.setInt(8, a.getAssetBannedDays());
 
-		// sql query for insertion
-		String sql1 = "Insert into Asset ( assetName,assetType,assetDescription,assetDateAdded,assetAvailability,assetLendingPeriod,assetLateReturnFee,assetBannedDays) values(?,?,?,?,?,?,?,?)";
-
-		
-
-			ps = conn.prepareStatement(sql1);
-
-			ps.setString(1, a.getAssetName());
-			ps.setString(2, a.getAssetType());
-			ps.setString(3, a.getAssetDescription());
-			ps.setString(4, a.getAssetDateAdded());
-			ps.setInt(5, a.getAssetAvailable());
-			ps.setInt(6, a.getAssetLendingPeriod());
-			ps.setFloat(7, a.getAssetLateReturnFee());
-			ps.setInt(8, a.getAssetBannedDays());
-
-			int i = ps.executeUpdate();
+			int i = pinsert.executeUpdate();
 
 			if (i > 0)
 				System.out.println("Insert successful");
@@ -53,20 +83,19 @@ public class AsssetDaoImpl implements AssetDaoIntf {
 		List<Asset> alist = new ArrayList<>();
 		Asset a = null;
 
-		// sql query for fetching all the assets
-		String sql = "Select * from Asset";
-		PreparedStatement ps = null;
+		
 		ResultSet rs = null;
 
 		
-			ps = conn.prepareStatement(sql);
-			rs = ps.executeQuery();
+		
+			rs = pselectAll.executeQuery();
 			if (rs.next()) {
-				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+				LocalDate date=rs.getDate(5).toLocalDate();
+				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 						rs.getInt(7), rs.getInt(8), rs.getInt(9));
 				alist.add(a);
 				while (rs.next()) {
-					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 							rs.getInt(7), rs.getInt(8), rs.getInt(9));
 					alist.add(a);
 
@@ -82,22 +111,21 @@ public class AsssetDaoImpl implements AssetDaoIntf {
 	// getting assets by name
 	public List<Asset> getAssetByName(String name) throws AssetNotFoundException, SQLException {
 
-		PreparedStatement ps = null;
+		
 		List<Asset> alist = new ArrayList<>();
 		ResultSet rs = null;
 		Asset a = null;
-		String sql = "Select * from Asset where assetName=?";
 		
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, name);
-			rs = ps.executeQuery();
+		pselectName.setString(1, name);
+			rs = pselectName.executeQuery();
 			if (rs.next()) {
-				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+				LocalDate date=rs.getDate(5).toLocalDate();
+				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 						rs.getInt(7), rs.getInt(8), rs.getInt(9));
 				alist.add(a);
 				while (rs.next()) {
 
-					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 							rs.getInt(7), rs.getInt(8), rs.getInt(9));
 					alist.add(a);
 				}
@@ -113,22 +141,21 @@ public class AsssetDaoImpl implements AssetDaoIntf {
 	public List<Asset> getAssetByCategory(String categoryName) throws AssetNotFoundException, SQLException {
 
 		
-		PreparedStatement ps = null;
+		
 		List<Asset> alist = new ArrayList<>();
 		Asset a = null;
 		ResultSet rs = null;
-		String sql = "Select * from Asset where assetType=?";
 		
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, categoryName);
-			rs = ps.executeQuery();
+		pselectCategory.setString(1, categoryName);
+			rs = pselectCategory.executeQuery();
 			if (rs.next()) {
-				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+				LocalDate date=rs.getDate(5).toLocalDate();
+				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 						rs.getInt(7), rs.getInt(8), rs.getInt(9));
 				alist.add(a);
 				while (rs.next()) {
 
-					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), date, rs.getInt(6),
 							rs.getInt(7), rs.getInt(8), rs.getInt(9));
 					alist.add(a);
 				}
@@ -144,22 +171,21 @@ public class AsssetDaoImpl implements AssetDaoIntf {
 //getting assets by date
 	public List<Asset> getAssetByDate(String date) throws AssetNotFoundException, SQLException {
 
-		PreparedStatement ps = null;
+		
 		List<Asset> alist = new ArrayList<>();
 		Asset a = null;
 		ResultSet rs = null;
-		String sql = "Select * from Asset where assetDateAdded=?";
-		
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, date);
-			rs = ps.executeQuery();
+	
+		pselectDate.setString(1, date);
+			rs = pselectDate.executeQuery();
 			if (rs.next()) {
-				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+				LocalDate objdate=rs.getDate(5).toLocalDate();
+				a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), objdate, rs.getInt(6),
 						rs.getInt(7), rs.getInt(8), rs.getInt(9));
 				alist.add(a);
 				while (rs.next()) {
 
-					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),
+					a = new Asset(rs.getString(2), rs.getString(3), rs.getString(4),objdate, rs.getInt(6),
 							rs.getInt(7), rs.getInt(8), rs.getInt(9));
 					alist.add(a);
 				}
